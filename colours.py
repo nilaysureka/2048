d14b68d261fb7c31ae70c8132b2b8f7bca0f2fc0
nilaysureka <139799789+nilaysureka@users.noreply.github.com>
2025-11-13 19:07:17 +0530

    Create colours.py

---
+++ colours.py
@@ -0,0 +1,21 @@
+GRID_COLOUR = "#a39489"
+EMPTY_CELL_COLOUR = "#c2b3a9"
+SCORE_LABEL_FONT = ("Verdana", 20)
+SCORE_FONT = ("Helvetica", 32, "bold")
+GAME_OVER_FONT = ("Helvetica", 48, "bold")
+GAME_OVER_FONT_COLOUR = "#ffffff"
+WINNER_BG = "#ffcc00"
+LOSER_BG = "#a39489"
+
+CELL_COLOURS = {
+    2: "#fcefe6",
+    4: "#f2e8cb",
+    8: "#f5b682",
+    16: "#f29446",
+    32: "#ff775c",
+    64: "#e64c2e",
+    128: "#ede291",
+    256: "#fce130",
+    512: "#ffdb4a",
+    1024: "#f0b922",
+    2048: "#fad74d"
