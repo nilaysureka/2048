431ca876a16f43ba28368d6e9153c81f9cace946
nilaysureka <139799789+nilaysureka@users.noreply.github.com>
2025-11-17 22:20:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import random
-import colors as c
+import colours as c
 
 class Game(tk.Frame):
     def __init__(self):
@@ -20,3 +20,82 @@ class Game(tk.Frame):
         self.master.bind("<Down>", self.down)
 
         self.mainloop()
+import tkinter as tk
+import random
+import colours as c
+
+class Game(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
+        self.grid()
+        self.master.title('2048')
+
+        self.main_grid = tk.Frame(
+            self, bg=c.GRID_COLOUR, bd=3, width=400, height=400)
+        self.main_grid.grid(pady=(80, 0))
+        self.make_GUI()
+        self.start_game()
+
+        self.master.bind("<Left>", self.left)
+        self.master.bind("<Right>", self.right)
+        self.master.bind("<Up>", self.up)
+        self.master.bind("<Down>", self.down)
+
+        self.mainloop()
+
+    def make_GUI(self):
+        # make grid
+        self.cells = []
+        for i in range(4):
+            row = []
+            for j in range(4):
+                cell_frame = tk.Frame(
+                    self.main_grid,
+                    bg=c.EMPTY_CELL_COLOR,
+                    width=100,
+                    height=100)
+                cell_frame.grid(row=i, column=j, padx=5, pady=5)
+                cell_number = tk.Label(self.main_grid, bg=c.EMPTY_CELL_COLOR)
+                cell_number.grid(row=i, column=j)
+                cell_data = {"frame": cell_frame, "number": cell_number}
+                row.append(cell_data)
+            self.cells.append(row)
+
+        # make score header
+        score_frame = tk.Frame(self)
+        score_frame.place(relx=0.5, y=40, anchor="center")
+        tk.Label(
+            score_frame,
+            text="Score",
+            font=c.SCORE_LABEL_FONT).grid(
+            row=0)
+        self.score_label = tk.Label(score_frame, text="0", font=c.SCORE_FONT)
+        self.score_label.grid(row=1)
+
+
+    def start_game(self):
+        # create matrix of zeroes
+        self.matrix = [[0] * 4 for _ in range(4)]
+
+        # fill 2 random cells with 2s
+        row = random.randint(0, 3)
+        col = random.randint(0, 3)
+        self.matrix[row][col] = 2
+        self.cells[row][col]["frame"].configure(bg=c.CELL_COLORS[2])
+        self.cells[row][col]["number"].configure(
+            bg=c.CELL_COLORS[2],
+            fg=c.CELL_NUMBER_COLORS[2],
+            font=c.CELL_NUMBER_FONTS[2],
+            text="2")
+        while(self.matrix[row][col] != 0):
+            row = random.randint(0, 3)
+            col = random.randint(0, 3)
+        self.matrix[row][col] = 2
+        self.cells[row][col]["frame"].configure(bg=c.CELL_COLORS[2])
+        self.cells[row][col]["number"].configure(
+            bg=c.CELL_COLORS[2],
+            fg=c.CELL_NUMBER_COLORS[2],
+            font=c.CELL_NUMBER_FONTS[2],
+            text="2")
+
+        self.score = 0
