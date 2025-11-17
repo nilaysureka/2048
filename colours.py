a377050b9c463a4d4ac447ef919e511db08be14d
nilaysureka <139799789+nilaysureka@users.noreply.github.com>
2025-11-17 22:19:42 +0530

    Update colours.py

--- colours.py
+++ colours.py
@@ -1,13 +1,13 @@
-GRID_COLOR = "#a39489"
-EMPTY_CELL_COLOR = "#c2b3a9"
+GRID_Colour = "#a39489"
+EMPTY_CELL_Colour = "#c2b3a9"
 SCORE_LABEL_FONT = ("Verdana", 20)
 SCORE_FONT = ("Helvetica", 32, "bold")
 GAME_OVER_FONT = ("Helvetica", 48, "bold")
-GAME_OVER_FONT_COLOR = "#ffffff"
+GAME_OVER_FONT_Colour = "#ffffff"
 WINNER_BG = "#ffcc00"
 LOSER_BG = "#a39489"
 
-CELL_COLORS = {
+CELL_Colours = {
     2: "#fcefe6",
     4: "#f2e8cb",
     8: "#f5b682",
@@ -21,7 +21,7 @@ CELL_COLORS = {
     2048: "#fad74d"
 }
 
-CELL_NUMBER_COLORS = {
+CELL_NUMBER_Colours = {
     2: "#695c57",
     4: "#695c57",
     8: "#ffffff",
